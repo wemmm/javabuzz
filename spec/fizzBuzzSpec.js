2787883dbdc95ca15d8b3c049af9011ebaea3652
wemmm <jenny.wem@gmail.com>
2017-06-05 16:32:40 +0100

    Initial commit

---
+++ spec/fizzBuzzSpec.js
@@ -0,0 +1,13 @@
+describe('Javabuzz', function() {
+
+  var javabuzz;
+
+  describe('knows what a number is', function(){
+
+    it('is divisble by 3', function() {
+      javabuzz = new Javabuzz();
+    });
+
+  });
+
+});
